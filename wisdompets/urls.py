656eac8b6555387ab8db56d5bda3e6e884336796
Idiel99 <idielguerra@hotmail.com>
2020-02-26 21:31:40 -0500

    pc commit

---
+++ wisdompets/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from adoptions import views
+from django.conf.urls import url
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+]
